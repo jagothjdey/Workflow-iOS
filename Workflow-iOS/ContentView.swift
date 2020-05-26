1cf9054181bdce00d4ecb60e2547915434150101
dey <jagothjyotidey91@gmail.com>
2020-05-26 13:05:40 +0200

    try 3

--- Workflow-iOS/ContentView.swift
+++ Workflow-iOS/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I am inside master branch. Check workflow try 2")
+        Text("I am inside master branch. Check workflow try 3")
     }
 }
 
