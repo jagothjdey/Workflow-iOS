93198947db6a474588d74882d2247ecef50dff62
dey <jagothjyotidey91@gmail.com>
2020-05-26 10:12:39 +0200

    Initial Commit

---
+++ Workflow-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workflow-iOS
+//
+//  Created by Jagoth Jyoti Dey on 26.05.20.
+//  Copyright © 2020 Jagoth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
